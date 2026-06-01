c3532c872b54a341bc94880d8884884883eac4e2
Vinicius Ceciliato <viniceciliato2008@gmail.com>
2026-06-01 10:22:23 -0300

    Update api.php

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -18,4 +18,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('teste', function() {});
+Route::get('adicionar-aluno', function () {
+    return 'teste';
+});
+
+Route::prefix('/aluno')->group(function () {
+    Route::get('/index', function(){});
+    Route::get('/adicionar', function(){});
+    Route::get('/remover', function(){});
+    Route::get('/atualizar', function(){});
+    Route::get('/listar', function(){});
+});
